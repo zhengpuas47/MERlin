ffa2f27710449aa3608ba93264474afbb4d315ab
zhengpuas47 <zhengpuas47@gmail.com>
2023-05-24 18:06:15 -0400

    name py310 as v1.7

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ CLASSIFIERS = [
     "Operating System :: Unix",
     "Operating System :: MacOS :: MacOS X",
     "License  :: Restricted use",
-    "Programming Language :: Python :: 3.8",
+    "Programming Language :: Python :: 3.10",
     "Topic :: Scientific/Engineering :: Bio-Informatics",
 ]
 
@@ -17,10 +17,10 @@ install_requires = [line.rstrip() for line in open(
 
 setuptools.setup(
     name="merlin",
-    version="0.1.6b",
+    version="0.1.7",
     description="MERFISH decoding software",
-    author="George Emanuel",
-    author_email="emanuega0@gmail.com",
+    author="George Emanuel, Pu Zheng",
+    author_email="emanuega0@gmail.com, zhengpuas47@gmail.com",
     license="Restricted use",
     packages=setuptools.find_packages(),
     install_requires=install_requires,
@@ -29,3 +29,6 @@ setuptools.setup(
     },
     classifiers=CLASSIFIERS
 )
+## Version 1.7
+# Updated by Pu Zheng, 2023.5.24
+# modify all packages to match python 3.10
